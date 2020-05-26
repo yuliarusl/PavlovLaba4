d8be8f17333c21c8a0ad1c4f820bcb65e4efa1ab
Ulia <Kscom6614@gmail.com>
2020-05-26 19:18:39 +0700

    Extended version controller

---
+++ Controllers/VersionController.cs
@@ -0,0 +1,25 @@
+﻿using WebApplication3.Models;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            var versionInfo = new Version
+            {
+                Company = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyCompanyAttribute>().Company,
+                Product = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyProductAttribute>().Product,
+                ProductVersion = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion
+            };
+            return Ok(versionInfo);
+        }
+    }
+
+}
---
+++ Models/Version.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class Version
+    {
+        public string Company { get; set; }
+        public string ProductVersion { get; set; }
+        public string Product { get; set; }
+    }
+
+}
