967544d6471f22a36a2ba985d15193aaec9d99e3
Ulia <Kscom6614@gmail.com>
2020-05-26 19:24:10 +0700

    Rebuild controller. Add IStorage, memCache

---
+++ Storage/Exceptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Storage
+{
+    [System.Serializable]
+    public class IncorrectPhonesDataException : System.Exception
+    {
+        public IncorrectPhonesDataException() { }
+        public IncorrectPhonesDataException(string message) : base(message) { }
+        public IncorrectPhonesDataException(string message, System.Exception inner) : base(message, inner) { }
+        protected IncorrectPhonesDataException(
+            System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
